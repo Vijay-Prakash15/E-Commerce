19a1464edc342d64ee0f7d47557ff1c990856085
Vijay Prakash <vijaypr82701@gmail.com>
2026-07-08 15:01:13 +0530

    Fix CORS for production

--- server/server.js
+++ server/server.js
@@ -47,15 +47,8 @@ const PORT = process.env.PORT || 5000;
 // 🌐 CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST", "DELETE", "PUT"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "Cache-Control",
-      "Expires",
-      "Pragma",
-    ],
     credentials: true,
   })
 );
